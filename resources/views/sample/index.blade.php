77f8d13178bca63df614e151c30ad4c4b3ce24cb
junifar <junifar@gmail.com>
2017-05-17 03:26:05 +0700

    Adding Layout

--- resources/views/sample/index.blade.php
+++ resources/views/sample/index.blade.php
@@ -1,1 +1,20 @@
-<h1>Sample</h1>
+@extends('layout')
+
+@section('title')
+    Test Index
+@endsection
+
+@section('content-header')
+    <h1>
+        Dashboard
+        <small>Version 2.0</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+    </ol>
+@endsection
+
+@section('content')
+    <h1>Sample</h1>
+@endsection
