03e84361ef31c3f9e5eeb02113b18e027f220aee
junifar <junifar@gmail.com>
2017-05-18 23:09:31 +0700

    Search Design

--- resources/views/kecamatan/index.blade.php
+++ resources/views/kecamatan/index.blade.php
@@ -23,6 +23,16 @@
                 </div>
                 <div class="box-body">
                     <div class="row">
+                        <div class="col-md-5 col-md-offset-3">
+                            <div class="input-group input-group-sm">
+                                <input type="text" class="form-control" placeholder="Search keyword">
+                                <span class="input-group-btn">
+                                    <button type="button" class="btn btn-info btn-flat">Search</button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-md-12">
                             <div class="mailbox-controls">
                                 <!-- /.btn-group -->
@@ -38,53 +48,35 @@
                                 <!-- /.pull-right -->
                             </div>
                             <div class="table-responsive mailbox-messages">
-                                <table class="table table-bordered">
+                                <table class="table table-bordered table-hover">
                                     <tbody>
                                     <tr>
                                         <th style="width: 10px">#</th>
-                                        <th>Task</th>
-                                        <th>Progress</th>
-                                        <th style="width: 40px">Label</th>
+                                        <th>Kecamatan</th>
+                                        <th>Camat</th>
+                                        <th>Wakil Camat</th>
+                                        <th style="width: 70px">Action</th>
                                     </tr>
                                     <tr>
                                         <td>1.</td>
-                                        <td>Update software</td>
-                                        <td>
-                                            <div class="progress progress-xs">
-                                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-red">55%</span></td>
+                                        <td>Bekasi Utara</td>
+                                        <td>Suci</td>
+                                        <td>Putri</td>
+                                        <td><a href="#" class="btn btn-primary">Details</a></td>
                                     </tr>
                                     <tr>
                                         <td>2.</td>
-                                        <td>Clean database</td>
-                                        <td>
-                                            <div class="progress progress-xs">
-                                                <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-yellow">70%</span></td>
+                                        <td>Bekasi Utara</td>
+                                        <td>Suci</td>
+                                        <td>Putri</td>
+                                        <td><a href="#" class="btn btn-primary">Details</a></td>
                                     </tr>
                                     <tr>
                                         <td>3.</td>
-                                        <td>Cron job running</td>
-                                        <td>
-                                            <div class="progress progress-xs progress-striped active">
-                                                <div class="progress-bar progress-bar-primary" style="width: 30%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-light-blue">30%</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>4.</td>
-                                        <td>Fix and squish bugs</td>
-                                        <td>
-                                            <div class="progress progress-xs progress-striped active">
-                                                <div class="progress-bar progress-bar-success" style="width: 90%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-green">90%</span></td>
+                                        <td>Bekasi Utara</td>
+                                        <td>Suci</td>
+                                        <td>Putri</td>
+                                        <td><a href="#" class="btn btn-primary">Details</a></td>
                                     </tr>
                                     </tbody>
                                 </table>
